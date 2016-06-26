4f59fcf5ebe4fbfb7b6384efd7d3b33a09d4f809
Michael C. Saul <msaul [at] illinois.edu>
2016-06-26 13:23:41 -0500

    Added text clarifying that each function in this package is authored under the GNU GPL v3.

--- R/copy.to.excel.R
+++ R/copy.to.excel.R
@@ -1,6 +1,22 @@
 # copy.to.excel function
-# Michael C. Saul
+# Copyright 2016, Michael C. Saul
 # msaul [at] illinois.edu
+# 
+# This file is part of the msaul R package.
+# 
+# The msaul R package is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# The msaul R package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with the msaul R package  If not, see <http://www.gnu.org/licenses/>.
+# 
 # Started: 2015-06-26
 # Last edited: 2016-06-26
 #
--- R/hypergeo.overlap.test.R
+++ R/hypergeo.overlap.test.R
@@ -1,5 +1,22 @@
-# author: Michael C. Saul
-# email: msaul [at] illinois.edu
+# hypergeo.overlap.test function
+# Copyright 2016, Michael C. Saul
+# msaul [at] illinois.edu
+# 
+# This file is part of the msaul R package.
+# 
+# The msaul R package is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# The msaul R package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with the msaul R package  If not, see <http://www.gnu.org/licenses/>.
+# 
 # project_start: 2015-04-29
 # last_edited: 2016-06-09
 #
