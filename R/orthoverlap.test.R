24ad728a826e445c1efa766a2f0e15b1f84ad9c5
Michael C. Saul <msaul [at] illinois.edu>
2016-11-10 16:26:50 -0600

    Changed the orthoverlap.test function to assume hypergeometric instead of binomal normal approximations for each orthogroup. This is consistent with a list drawn without replacement. The change to the test is minor and technical.

--- R/orthoverlap.test.R
+++ R/orthoverlap.test.R
@@ -24,7 +24,7 @@
 #'
 #' For four character vectors (list1.s1, list2.s2, orthodb.index.s1, orthodb.index.s2), this function computes a randomization-based p-value of the overlap.
 #' It can test for either overrepresentation or underrepresention.
-#' Orthoverlap uses a normal approximation of the binomial distribution to make scores from different distributions directly comparable.
+#' Orthoverlap uses a normal approximation of the hypergeometric distribution to make scores from different distributions directly comparable.
 #' It calculates the p-value by comparing the Pearson correlation coefficient derived from data with orthology to a sample with orthology bootstrapped.
 #'
 #' @author Michael C. Saul
@@ -71,12 +71,16 @@ orthoverlap.test = function(list1.s1, list2.s2, orthodb.index.s1, orthodb.index.
   orthodb.set = data.frame(row.names=orthodb.set,
                            size.s1 = rep(0, times = length(orthodb.set)),
                            hits.s1 = rep(0, times = length(orthodb.set)),
+                           numerator.s1 = rep(0.0, times = length(orthodb.set)),
+                           denominator.s1 = rep(0.0, times = length(orthodb.set)),
                            score.s1 = rep(0.0, times = length(orthodb.set)),
-                           binom.p.s1 = rep(0.0, times = length(orthodb.set)),
+                           hypergeo.p.s1 = rep(0.0, times = length(orthodb.set)),
                            size.s2 = rep(0, times = length(orthodb.set)),
                            hits.s2 = rep(0, times = length(orthodb.set)),
+                           numerator.s2 = rep(0.0, times = length(orthodb.set)),
+                           denominator.s2 = rep(0.0, times = length(orthodb.set)),
                            score.s2 = rep(0.0, times = length(orthodb.set)),
-                           binom.p.s2 = rep(0.0, times = length(orthodb.set)))
+                           hypergeo.p.s2 = rep(0.0, times = length(orthodb.set)))
   
   # Getting counts of total genes in each orthogroup
   odb.n.s1 = aggregate(id ~ orthodb, data = orthodb.index.s1, FUN = length)
@@ -94,8 +98,10 @@ orthoverlap.test = function(list1.s1, list2.s2, orthodb.index.s1, orthodb.index.
   row.names(list1.n) = list1.n$orthodb
   list1.genes.in.orthodb = sum(list1.n$id)
   list1.n$bg.sum = odb.n.s1[list1.n$orthodb,"id"]
-  list1.p = sum(list1.n$id) / nrow(orthodb.index.s1) # need to figure out exactly what this should be
+  list1.p = sum(list1.n$id) / nrow(orthodb.index.s1)
   orthodb.set[row.names(list1.n),"hits.s1"] = list1.n$id
+  total.s1 = sum(orthodb.set$size.s1)
+  hits.s1 = sum(orthodb.set$hits.1)
   
   # Getting counts of genes in each orthogroup in list2.s2
   list2.odb = data.frame(id = as.character(list2))
@@ -105,16 +111,31 @@ orthoverlap.test = function(list1.s1, list2.s2, orthodb.index.s1, orthodb.index.
   row.names(list2.n) = list2.n$orthodb
   list2.genes.in.orthodb = sum(list2.n$id)
   list2.n$bg.sum = odb.n.s2[list2.n$orthodb,"id"]
-  list2.p = sum(list2.n$id) / nrow(orthodb.index.s2) # need to figure out exactly what this should be
+  list2.p = sum(list2.n$id) / nrow(orthodb.index.s2)
   orthodb.set[row.names(list2.n),"hits.s2"] = list2.n$id
+  total.s2 = sum(orthodb.set$size.s2)
+  hits.s2 = sum(orthodb.set$hits.s2)
   
   # Integrating information from each individual species in the orthoDB data frame into Z-Scores
-  orthodb.set$score.s1 = (orthodb.set$hits.s1 - (orthodb.set$size.s1 * list1.p)) / sqrt(orthodb.set$size.s1 * list1.p * (1 - list1.p))
-  orthodb.set$score.s2 = (orthodb.set$hits.s2 - (orthodb.set$size.s2 * list2.p)) / sqrt(orthodb.set$size.s2 * list2.p * (1 - list2.p))
+  # Note: approximated from the hypergeometric distribution
+  orthodb.set$numerator.s1 = orthodb.set$hits.s1 - ((orthodb.set$size.s1 * hits.s1) / total.s1)
+  orthodb.set$denominator.s1 = sqrt((orthodb.set$size.s1 * total.s1 * (total.s1 - hits.s1) * (total.s1 - orthodb.set$size.s1)) / ((total.s1 ^ 2) * (total.s1 - 1)))
+  orthodb.set$score.s1 = orthodb.set$numerator.s1 / orthodb.set$denominator.s1
+  orthodb.set$numerator.s2 = orthodb.set$hits.s2 - ((orthodb.set$size.s2 * hits.s2) / total.s2)
+  orthodb.set$denominator.s2 = sqrt((orthodb.set$size.s2 * total.s2 * (total.s2 - hits.s2) * (total.s2 - orthodb.set$size.s2)) / ((total.s2 ^ 2) * (total.s2 - 1)))
+  orthodb.set$score.s2 = orthodb.set$numerator.s2 / orthodb.set$denominator.s2
   
   for (j in 1:nrow(orthodb.set)) {
-    orthodb.set[j,"binom.p.s1"] = sum(dbinom(orthodb.set[j,"hits.s1"]:orthodb.set[j,"size.s1"],orthodb.set[j,"size.s1"],list1.p))
-    orthodb.set[j,"binom.p.s2"] = sum(dbinom(orthodb.set[j,"hits.s2"]:orthodb.set[j,"size.s2"],orthodb.set[j,"size.s2"],list2.p))
+    orthodb.set[j,"hypergeo.p.s1"] = phyper(q = orthodb.set[j,"hits.s1"],
+                                            m = hits.s1,
+                                            n = (total.s1 - hits.s1),
+                                            k = orthodb.set[j,"size.s1"],
+                                            lower.tail = FALSE)
+    orthodb.set[j,"hypergeo.p.s2"] = phyper(q = orthodb.set[j,"hits.s2"],
+                                            m = hits.s2,
+                                            n = (total.s2 - hits.s2),
+                                            k = orthodb.set[j,"size.s2"],
+                                            lower.tail = FALSE)
   }
   
   orthodb.set$individual.score = orthodb.set$score.s1 * orthodb.set$score.s2
@@ -181,6 +202,10 @@ orthoverlap.test = function(list1.s1, list2.s2, orthodb.index.s1, orthodb.index.
   test.list[["orthodb.set"]] = orthodb.set
   test.list[["p.s1"]] = list1.p
   test.list[["p.s2"]] = list2.p
+  test.list[["total.s1"]] = total.s1
+  test.list[["total.s2"]] = total.s2
+  test.list[["hits.s1"]] = hits.s1
+  test.list[["hits.s2"]] = hits.s2
   attr(test.list,"class") = "htest"
   return(test.list)
 }
